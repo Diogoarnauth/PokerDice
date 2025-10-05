e84c064f9c5a2a83a3b62fb98407b4c7dbb65c80
RenataCastanheira <renatacastanheira.atb23@gmail.com>
2025-10-05 10:11:07 +0100

    - Adição do LobbiesService.kt, assim como as classes que constituem os seus tipos de utilização no model (leave, join, result,...), assim como a sua classe geral no domain

---
+++ src/main/kotlin/HTTP/model/JoinLobbyResult.kt
@@ -0,0 +1,12 @@
+
+/**
+ * Resultado de POST /lobbies/{id}/players (entrar num lobby).
+ * Cobre todos os cenários: sucesso, inexistente, cheio, ou jogador já presente.
+ */
+
+sealed interface JoinLobbyResult {
+    object Ok : JoinLobbyResult
+    object NotFound : JoinLobbyResult
+    object Full : JoinLobbyResult
+    object AlreadyIn : JoinLobbyResult
+}

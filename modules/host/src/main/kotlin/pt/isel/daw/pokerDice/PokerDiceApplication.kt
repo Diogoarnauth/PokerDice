206cae2c1fa4b34af4b1e5dbbca64cec97f612f1
Diogo Arnauth Fernandes Leitão <dioarnauth@gmail.com>
2025-10-10 15:09:06 +0100

    erros corrigidos, gradles a funcionar

--- modules/host/src/main/kotlin/pt/isel/daw/pokerDice/PokerDiceApplication.kt
+++ modules/host/src/main/kotlin/pt/isel/daw/pokerDice/PokerDiceApplication.kt
@@ -8,21 +8,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import pt.isel.daw.pokerDice.domain.Invite.InviteDomainConfig
+import pt.isel.daw.pokerDice.domain.Invite.Sha256InviteEncoder
 //import pt.isel.daw.pokerDice.repository.jdbi.configureWithAppRequirements // se tiveres esta função
 import pt.isel.daw.pokerDice.domain.players.PlayersDomainConfig
+import pt.isel.daw.pokerDice.domain.players.Sha256TokenEncoder
 import kotlin.time.Duration.Companion.hours
 
 @SpringBootApplication
 class PokerDiceApplication {
 
-   /* @Bean
+    @Bean
     fun jdbi(): Jdbi =
         Jdbi.create(
             PGSimpleDataSource().apply {
                 setURL(System.getenv("DB_URL") ?: "jdbc:postgresql://localhost:5432/pokerdice?user=postgres&password=postgres")
             }
         ) // podes adicionar .configureWithAppRequirements() se existir
-*/
+
+    @Bean
+    fun tokenEncoder() = Sha256TokenEncoder()
+
+    @Bean
+    fun inviteEncoder() = Sha256InviteEncoder()
+
     @Bean
     fun passwordEncoder() = BCryptPasswordEncoder()
 
@@ -36,6 +45,16 @@ class PokerDiceApplication {
             tokenTtl = 24.hours,
             tokenRollingTtl = 1.hours,
             maxTokensPerUser = 3,
+        )
+
+    @Bean
+    fun inviteDomainConfig() =
+        InviteDomainConfig(
+            validState = "pending",
+            expireInviteTime = 24.hours,
+            expiredState = "expired",
+            usedState = "used",
+            declinedState = "declined",
         )
 }
 
